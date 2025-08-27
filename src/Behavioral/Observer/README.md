45ce0e09bb29c8950fef6914b3b00db2db6cae53
beeboy98 <niko.mehilainen@gmail.com>
2025-08-27 10:30:25 +0300

    add README.md to assignment 4 Observer.

---
+++ src/Behavioral/Observer/README.md
@@ -0,0 +1,3 @@
+A simple weather station built using the Observer design pattern. The weather station acts as the publisher and runs in its own thread, while the thermometers act as listeners (observers) that receive updates and print the latest temperature from the station.
+
+Assignment: https://github.com/vesavvo/design_patterns/blob/master/markdown/assignments/observer.md
---
+++ src/Behavioral/Observer/WeatherStation.java
@@ -0,0 +1,50 @@
+package Behavioral.Observer;
+
+import java.util.Random;
+
+public class WeatherStation extends Subject {
+    private int temperature;
+
+    public WeatherStation() {
+        Random rand = new Random();
+        temperature = rand.nextInt(58);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1500); //sleep for 1.5 seconds.
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            changeTemperature();
+        }
+    }
+
+    private void changeTemperature() {
+        Random rand = new Random();
+        int randomTemp = rand.nextInt(2);
+        int tempTemperature = temperature;
+
+        if(randomTemp == 0)  { tempTemperature++; } else { tempTemperature--; }
+        tempTemperature = clamp(tempTemperature, -273, 57);
+
+        if(temperature == tempTemperature) { return; }
+
+        temperature = tempTemperature;
+        notifyObservers();
+
+        //System.out.println("changeTemperature(); Temperature: " + temperature );
+    }
+
+    @Override
+    public String getStatus(){
+        return temperature + "°C.";
+    }
+
+    private int clamp (float val, float min, float max) {
+        return (int)(Math.max(min, Math.min(max, val)));
+    }
+}
